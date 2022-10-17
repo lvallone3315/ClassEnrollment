4f4a5d8cd85297712a347c75e49c63b26715178b
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2022-10-17 16:05:10 -0400

    Descriptive comments added for Class UI class definition and methods.

--- ClassEnrollment/ClassUI.cpp
+++ ClassEnrollment/ClassUI.cpp
@@ -1,12 +1,23 @@
-// PlayerUI.cpp
+// ClassUI.cpp
 //   Encapsulates the methods to read & write to the UI (for now, the console)
 
 #include <iostream>
 #include <stdlib.h>
 #include "ClassUI.h"
 
-// Player UI Class
-//   Singleton class (for now) - uses console
+/* Class UI Class
+ * Scope:
+ *   Handles all reads & writes to console (or in the future, a graphical window)
+ *   Singleton class (for now) - uses console
+ * 
+ * Methods:
+ *      getUserInput() writes prompt to console, blocks on user input (terminated by new line), returns input
+ *      writeOutput()  writes output to console & returns 0 indicating no error, 
+ *        in future write errors may contain an error code
+ * 
+ * Instance Variables:
+ *   None
+ */
 
 
 // empty constructor
@@ -14,14 +25,22 @@ ClassUI::ClassUI() {
 
 }
 
+
+// getUserInput()
+//   prompts user with string included in call
+//   waits for user input, echoes the input & returns it to caller
+
 string ClassUI::getUserInput(string prompt) {
     string userInput;
+
     writeOutput(prompt);
     cin >> userInput;
     writeOutput(userInput);
     return userInput;
 }
 
+// writeOutput()
+//   writes passed string to console
 int ClassUI::writeOutput(string output) {
     cout << output;
     return(0);
--- ClassEnrollment/ClassUI.h
+++ ClassEnrollment/ClassUI.h
@@ -3,6 +3,12 @@
 #include <string>
 using namespace std;  // fair programming practice, as could create scope issues, but don't feel like qualifying all reads & writes
 
+/*
+ * Header file for Class program UI
+ *    getUserInput() prompts user with specified string and returns user input
+ *    writeOutput() writes output to console and returns success (0)
+ */
+
 class ClassUI
 {
 public:
